bbe59e05f3a4a4d73f2da27f6722d1c2656a006a
sinlov <sinlovgmppt@gmail.com>
2016-11-03 17:11:34 +0800

    add MDLTest For temple

--- app/src/main/java/mdl/sinlov/android/demo/temp/MainActivity.java
+++ app/src/main/java/mdl/sinlov/android/demo/temp/MainActivity.java
@@ -1,13 +1,22 @@
 package mdl.sinlov.android.demo.temp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends MDLTestActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void bindView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void bindListener() {
+
     }
 }
